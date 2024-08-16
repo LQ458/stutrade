5da27c8ac8330e83946ab87434def629c2b0ef5e
LQ <143152892+LQ458@users.noreply.github.com>
2024-08-16 13:39:02 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,73 +1,46 @@
 import DBconnect from "../../../../libs/mongodb";
 import User from "../../../../models/user";
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from "bcryptjs";
 
-type Credentials = {
-  username: string;
-  password: string;
-};
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const handler = NextAuth({
+  session: {
+    strategy: 'jwt',
+    maxAge: 7 * 24 * 60 * 60,
+  },
+  pages: {
+    signIn: '/login',
+  },
   providers: [
-    Credentials({
-      name: "credentials",
-      credentials: {},
-      async authorize(credentials) {
-        const { username, password } = credentials as Credentials;
-        try {
-          await DBconnect();
-          const user = await User.findOne({ username });
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        await DBconnect;
+        const email = credentials?.email || '';
+        const user = await User.findOne({ email });
           if (!user) {
             return null;
           }
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (!passwordsMatch) {
-            return null;
-          }
-          return user;
-        } catch (error) {
-          console.log("Error: ", error);
+        const passwordCorrect = await bcrypt.compare(
+          credentials?.password || '',
+          user.password
+        );
+
+        if (passwordCorrect) {
+          return {
+            id: user._id,
+            user
+          };
         }
+
+        return null;
       },
     }),
   ],
-  session: {
-    strategy: "jwt",
-    maxAge: 3 * 24 * 60 * 60,
-  },
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    async jwt({
-      token,
-      user,
-      trigger,
-      session,
-    }: {
-      token: any;
-      user: any;
-      trigger?: "signIn" | "signUp" | "update";
-      session?: any;
-    }) {
-      if (trigger === "update" && session) {
-        return {
-          ...token,
-          ...session.user,
-        };
-      }
-      return { ...token, ...user };
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          ...token,
-        },
-      };
-    },
-  },
 });
+
+export { handler as GET, handler as POST };
